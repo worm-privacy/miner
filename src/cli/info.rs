1480ae3a463fe73eb9c265d5956fccef2cbca7e7
sarah kaviani <sarakvn@gmail.com>
2025-07-20 20:27:59 +0400

    chore(cli/info): remove unused imports and commented code for clarity

--- src/cli/info.rs
+++ src/cli/info.rs
@@ -1,39 +1,13 @@
 use structopt::StructOpt;
 
-use std::{path::PathBuf, process::Command, time::Duration};
-
-// use alloy_rlp::Decodable;
-use crate::fp::{Fp, FpRepr};
-use anyhow::anyhow;
-use ff::{Field, PrimeField};
-// use poseidon2::poseidon2;
-// use serde::{Deserialize, Serialize};
-// use serde_json::json;
-use crate::networks::{NETWORKS, Network};
-use crate::poseidon2::poseidon2;
-use crate::utils::{RapidsnarkOutput, find_burn_key, generate_burn_address, input_file};
-use alloy::rlp::Encodable;
-use worm_witness_gens::generate_proof_of_burn_witness_file;
-// use alloy::sol;
-use tempfile::tempdir;
+use crate::networks::NETWORKS;
 
 use crate::utils::{BETH, WORM};
 
 use alloy::{
-    // eips::BlockId,
-    // hex::ToHexExt,
-    network::TransactionBuilder,
-    primitives::{
-        B256,
-        U256,
-        // map::HashMap,
-        utils::{format_ether},
-    },
-    providers::{Provider, ProviderBuilder},
-    // rlp::RlpDecodable,
-    // rpc::types::TransactionRequest,
+    primitives::{U256, utils::format_ether},
+    providers::ProviderBuilder,
     signers::local::PrivateKeySigner,
-    // transports::http::reqwest,
 };
 
 #[derive(StructOpt)]
