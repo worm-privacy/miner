f3a85d9b808ec2121aaaa481597ea672052840c9
sarah kaviani <sarakvn@gmail.com>
2025-07-20 20:29:02 +0400

    chore(cli/generate_witness): remove commented blocks and unused use statements

--- src/cli/generate_witness.rs
+++ src/cli/generate_witness.rs
@@ -1,40 +1,8 @@
 use structopt::StructOpt;
 
-use std::{path::PathBuf, process::Command, time::Duration};
+use std::path::PathBuf;
 
-// use alloy_rlp::Decodable;
-use crate::fp::{Fp, FpRepr};
-use anyhow::anyhow;
-use ff::{Field, PrimeField};
-// use poseidon2::poseidon2;
-// use serde::{Deserialize, Serialize};
-// use serde_json::json;
-use crate::networks::{NETWORKS, Network};
-use crate::poseidon2::poseidon2;
-use crate::utils::{RapidsnarkOutput, find_burn_key, generate_burn_address, input_file};
-use alloy::rlp::Encodable;
 use worm_witness_gens::generate_proof_of_burn_witness_file;
-// use alloy::sol;
-use tempfile::tempdir;
-
-use crate::utils::{BETH, WORM};
-
-use alloy::{
-    eips::BlockId,
-    hex::ToHexExt,
-    network::TransactionBuilder,
-    primitives::{
-        B256,
-        U256,
-        // map::HashMap,
-        utils::{format_ether, parse_ether},
-    },
-    providers::{Provider, ProviderBuilder},
-    // rlp::RlpDecodable,
-    rpc::types::TransactionRequest,
-    signers::local::PrivateKeySigner,
-    // transports::http::reqwest,
-};
 
 #[derive(StructOpt)]
 pub struct GenerateWitnessProofOfBurnOpt {
