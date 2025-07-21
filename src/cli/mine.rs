d5a783c5d33bda4c1f4223b50338e125fd726322
sarah kaviani <sarakvn@gmail.com>
2025-07-21 14:50:31 +0400

    fix(miner): wrap async loop body in try-like block to catch and handle errors

--- src/cli/mine.rs
+++ src/cli/mine.rs
@@ -9,6 +9,7 @@ use alloy::{
 };
 use std::time::Duration;
 use structopt::StructOpt;
+use anyhow::{Result};
 
 #[derive(StructOpt)]
 pub struct MineOpt {
@@ -57,61 +58,48 @@ impl MineOpt {
             // userShare / (userShare + totalShare) * 50 * assumedWormPrice >= userShare
             // if totalShare > 0 => userShare = 50 * assumedWormPrice - totalShare
             // if totalShare = 0 => userShare = minimumBethPerEpoch
+            
             loop {
-                let epoch = worm.currentEpoch().call().await?;
-                let previous_epoch = epoch.saturating_sub(U256::ONE);
-                let previous_total_share = worm.epochTotal(previous_epoch).call().await?;
-                let current_total_share = worm.epochTotal(epoch).call().await?;
-                let current_user_share = worm.epochUser(epoch, addr).call().await?;
-                let user_share = std::cmp::min(
-                    std::cmp::max(
-                        if current_total_share.is_zero() {
-                            minimum_beth_per_epoch
-                        } else {
-                            (U256::from(50) * assumed_worm_price)
-                                .saturating_sub(previous_total_share)
-                        },
-                        minimum_beth_per_epoch,
-                    ),
-                    maximum_beth_per_epoch,
-                )
-                .saturating_sub(current_user_share);
-
-                let num_epochs_to_check = std::cmp::min(epoch, U256::from(10));
-                let claimable_worm = worm
-                    .calculateMintAmount(
-                        epoch.saturating_sub(num_epochs_to_check),
-                        num_epochs_to_check,
-                        addr,
+                let result = async {
+                    let epoch = worm.currentEpoch().call().await?;
+                    let previous_epoch = epoch.saturating_sub(U256::ONE);
+                    let previous_total_share = worm.epochTotal(previous_epoch).call().await?;
+                    let current_total_share = worm.epochTotal(epoch).call().await?;
+                    let current_user_share = worm.epochUser(epoch, addr).call().await?;
+                    let user_share = std::cmp::min(
+                        std::cmp::max(
+                            if current_total_share.is_zero() {
+                                minimum_beth_per_epoch
+                            } else {
+                                (U256::from(50) * assumed_worm_price)
+                                    .saturating_sub(previous_total_share)
+                            },
+                            minimum_beth_per_epoch,
+                        ),
+                        maximum_beth_per_epoch,
                     )
-                    .call()
-                    .await?;
+                    .saturating_sub(current_user_share);
 
-                if user_share >= minimum_beth_per_epoch {
-                    println!(
-                        "Participating {} x {} for epochs {}..{}",
-                        self.future_epochs,
-                        format_ether(user_share),
-                        epoch,
-                        epoch + U256::from(self.future_epochs)
-                    );
-                    let receipt = worm
-                        .participate(user_share, U256::from(self.future_epochs as u64))
-                        .send()
-                        .await?
-                        .get_receipt()
+                    let num_epochs_to_check = std::cmp::min(epoch, U256::from(10));
+                    let claimable_worm = worm
+                        .calculateMintAmount(
+                            epoch.saturating_sub(num_epochs_to_check),
+                            num_epochs_to_check,
+                            addr,
+                        )
+                        .call()
                         .await?;
-                    if receipt.status() {
-                        println!("Success!");
-                    }
 
-                    if !(epoch % U256::from(10)).is_zero() && claimable_worm.is_zero() {
-                        println!("Claiming WORMs...");
+                    if user_share >= minimum_beth_per_epoch {
+                        println!(
+                            "Participating {} x {} for epochs {}..{}",
+                            self.future_epochs,
+                            format_ether(user_share),
+                            epoch,
+                            epoch + U256::from(self.future_epochs)
+                        );
                         let receipt = worm
-                            .claim(
-                                epoch.saturating_sub(num_epochs_to_check),
-                                num_epochs_to_check,
-                            )
+                            .participate(user_share, U256::from(self.future_epochs as u64))
                             .send()
                             .await?
                             .get_receipt()
@@ -119,22 +107,47 @@ impl MineOpt {
                         if receipt.status() {
                             println!("Success!");
                         }
+
+                        if !(epoch % U256::from(10)).is_zero() && claimable_worm.is_zero() {
+                            println!("Claiming WORMs...");
+                            let receipt = worm
+                                .claim(
+                                    epoch.saturating_sub(num_epochs_to_check),
+                                    num_epochs_to_check,
+                                )
+                                .send()
+                                .await?
+                                .get_receipt()
+                                .await?;
+                            if receipt.status() {
+                                println!("Success!");
+                            }
+                        }
                     }
+
+                    let eth_balance = provider.get_balance(addr).await?;
+                    let beth_balance = beth.balanceOf(addr).call().await?;
+                    let worm_balance = worm.balanceOf(addr).call().await?;
+
+                    println!(
+                        "ETH: {} BETH: {} WORM: {} Claimable WORM: {}",
+                        format_ether(eth_balance),
+                        format_ether(beth_balance),
+                        format_ether(worm_balance),
+                        format_ether(claimable_worm)
+                    );
+
+                    tokio::time::sleep(Duration::from_secs(10)).await;
+                    Ok::<(), anyhow::Error>(())
                 }
-
-                let eth_balance = provider.get_balance(addr).await?;
-                let beth_balance = beth.balanceOf(addr).call().await?;
-                let worm_balance = worm.balanceOf(addr).call().await?;
-
-                println!(
-                    "ETH: {} BETH: {} WORM: {} Claimable WORM: {}",
-                    format_ether(eth_balance),
-                    format_ether(beth_balance),
-                    format_ether(worm_balance),
-                    format_ether(claimable_worm)
-                );
-
-                tokio::time::sleep(Duration::from_secs(10)).await;
+                .await;
+                if let Err(e) = result {
+                    eprintln!("Something went wrong. Error: {}", e);
+                    tokio::time::sleep(Duration::from_secs(10)).await;
+                    continue;
+                }
+                println!("Success! Continuing...");
+                
             }
         }
         Ok(())
