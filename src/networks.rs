8a6565b5ee8c2ace8d14989b0a31a5d6b37a329d
sarah kaviani <sarakvn@gmail.com>
2025-08-31 13:14:00 +0400

    update beth contract address in sepolia network

--- src/networks.rs
+++ src/networks.rs
@@ -24,7 +24,7 @@ lazy_static! {
                 "sepolia".into(),
                 Network {
                     rpc: "https://sepolia.drpc.org".parse().unwrap(),
-                    beth: address!("0x6fa638704a839B28C5B7168C8916AdD9F75CDEEc"),
+                    beth: address!("0x7f5ecb9184B271C22d1E52A739db2C302f77b7C2"),
                     worm: address!("0x557E9e7Eed905C7d21183Ec333dB2a8B1e34A85F"),
                 },
             ),
